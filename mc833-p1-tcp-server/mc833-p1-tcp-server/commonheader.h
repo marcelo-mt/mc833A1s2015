b71a315590de713c972ff5c49e7dde76106914e4
Marcelo Mingatos <marcelo.m.toledo@gmail.com>
2015-04-21 18:51:16 -0300

    Basic TCP client server and auxiliar methods/wrappers

---
+++ mc833-p1-tcp-server/mc833-p1-tcp-server/commonheader.h
@@ -0,0 +1,104 @@
+//
+//  commonheader.h
+//  mc833-p1-tcp-server
+//
+//  Created by Marcelo M Toledo on 4/21/15.
+//  Copyright (c) 2015 mc833. All rights reserved.
+//
+
+#ifndef mc833_p1_tcp_server_commonheader_h
+#define mc833_p1_tcp_server_commonheader_h
+
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+#include	<sys/time.h>	/* timeval{} for select() */
+#include	<time.h>		/* timespec{} for pselect() */
+#include	<netinet/in.h>	/* sockaddr_in{} and other Internet defns */
+#include	<arpa/inet.h>	/* inet(3) functions */
+#include	<errno.h>
+#include	<fcntl.h>		/* for nonblocking */
+#include	<netdb.h>
+#include	<signal.h>
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<string.h>
+#include	<sys/stat.h>	/* for S_xxx file mode constants */
+#include	<sys/uio.h>		/* for iovec{} and readv/writev */
+#include	<unistd.h>
+#include	<sys/wait.h>
+#include	<sys/un.h>		/* for Unix domain sockets */
+
+/* Following could be derived from SOMAXCONN in <sys/socket.h>, but many
+ kernels still #define it as 5, while actually supporting many more */
+#define	LISTENQ		1024	/* 2nd argument to listen() */
+
+/* Miscellaneous constants */
+#define	MAXLINE		4096	/* max text line length */
+
+/* Following shortens all the type casts of pointer arguments */
+#define	SA	struct sockaddr
+
+/* prototypes for our Unix wrapper functions: see {Sec errors} */
+void	*Calloc(size_t, size_t);
+void	 Close(int);
+void	 Dup2(int, int);
+int		 Fcntl(int, int, int);
+void	 Gettimeofday(struct timeval *, void *);
+//int		 Ioctl(int, int, void *);
+pid_t	 Fork(void);
+void	*Malloc(size_t);
+void	 Mktemp(char *);
+void	*Mmap(void *, size_t, int, int, int, off_t);
+int		 Open(const char *, int, mode_t);
+void	 Pipe(int *fds);
+ssize_t	 Read(int, void *, size_t);
+void	 Sigaddset(sigset_t *, int);
+void	 Sigdelset(sigset_t *, int);
+void	 Sigemptyset(sigset_t *);
+void	 Sigfillset(sigset_t *);
+int		 Sigismember(const sigset_t *, int);
+void	 Sigpending(sigset_t *);
+void	 Sigprocmask(int, const sigset_t *, sigset_t *);
+char	*Strdup(const char *);
+long	 Sysconf(int);
+void	 Sysctl(int *, u_int, void *, size_t *, void *, size_t);
+void	 Unlink(const char *);
+pid_t	 Wait(int *);
+pid_t	 Waitpid(pid_t, int *, int);
+void	 Write(int, void *, size_t);
+
+/* prototypes for our socket wrapper functions: see {Sec errors} */
+int		 Accept(int, SA *, socklen_t *);
+void	 Bind(int, const SA *, socklen_t);
+void	 Connect(int, const SA *, socklen_t);
+void	 Getpeername(int, SA *, socklen_t *);
+void	 Getsockname(int, SA *, socklen_t *);
+void	 Getsockopt(int, int, int, void *, socklen_t *);
+//int		 Isfdtype(int, int);
+void	 Listen(int, int);
+#ifdef	HAVE_POLL
+int		 Poll(struct pollfd *, unsigned long, int);
+#endif
+ssize_t	 Readline(int, void *, size_t);
+ssize_t	 Readn(int, void *, size_t);
+ssize_t	 Recv(int, void *, size_t, int);
+ssize_t	 Recvfrom(int, void *, size_t, int, SA *, socklen_t *);
+ssize_t	 Recvmsg(int, struct msghdr *, int);
+int		 Select(int, fd_set *, fd_set *, fd_set *, struct timeval *);
+void	 Send(int, const void *, size_t, int);
+void	 Sendto(int, const void *, size_t, int, const SA *, socklen_t);
+void	 Sendmsg(int, const struct msghdr *, int);
+void	 Setsockopt(int, int, int, const void *, socklen_t);
+void	 Shutdown(int, int);
+int		 Sockatmark(int);
+int		 Socket(int, int, int);
+void	 Socketpair(int, int, int, int *);
+void	 Writen(int, void *, size_t);
+
+void	 err_dump(const char *, ...);
+void	 err_msg(const char *, ...);
+void	 err_quit(const char *, ...);
+void	 err_ret(const char *, ...);
+void	 err_sys(const char *, ...);
+
+#endif
---
+++ mc833-p1-tcp-server/mc833-p1-tcp-server/movietcpsrv.c
@@ -0,0 +1,45 @@
+//
+//  movietcpsrv.c
+//  mc833-p1-tcp-server
+//
+//  Created by Marcelo M Toledo on 4/21/15.
+//  Copyright (c) 2015 mc833. All rights reserved.
+//
+
+#include "commonheader.h"
+
+int main(int argc, char **argv)
+{
+    int					listenfd, connfd;
+    struct sockaddr_in	servaddr;
+    char				buff[MAXLINE];
+    time_t				ticks;
+    
+    printf("SocketCreation...\n");
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+    
+    printf("Socket cleanup...\n");
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family      = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port        = htons(5001);	/* daytime server */
+    
+    printf("Binding...\n");
+    Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
+    
+    printf("Listening...\n");
+    Listen(listenfd, LISTENQ);
+    
+    printf("Waiting for connectin...");
+    for ( ; ; ) {
+        connfd = Accept(listenfd, (SA *) NULL, NULL);
+        
+        printf("Connection Accepted");
+        
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+        Write(connfd, buff, strlen(buff));
+        
+        Close(connfd);
+    }
+}
