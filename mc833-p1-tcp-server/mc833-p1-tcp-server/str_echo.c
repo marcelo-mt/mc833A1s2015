e4027503bfd29908659f23c62b8731a11e50d7e5
Marcelo Mingatos <marcelo.m.toledo@gmail.com>
2015-04-22 01:58:32 -0300

    TCP concurrent server with fork

--- mc833-p1-tcp-server/mc833-p1-tcp-server/str_echo.c
+++ mc833-p1-tcp-server/mc833-p1-tcp-server/str_echo.c
@@ -1,15 +1,21 @@
 #include	"commonheader.h"
 
-void
-str_echo(int sockfd)
+void str_echo(int sockfd)
 {
-	ssize_t		n;
-	char		line[MAXLINE];
-
-	for ( ; ; ) {
-		if ( (n = Readline(sockfd, line, MAXLINE)) == 0)
-			return;		/* connection closed by other end */
-
-		Writen(sockfd, line, n);
-	}
+    long		arg1, arg2;
+    ssize_t		n;
+    char		line[MAXLINE];
+    
+    for ( ; ; ) {
+        if ( (n = Readline(sockfd, line, MAXLINE)) == 0)
+            return;		/* connection closed by other end */
+        
+        if (sscanf(line, "%ld%ld", &arg1, &arg2) == 2)
+            snprintf(line, sizeof(line), "%ld\n", arg1 + arg2);
+        else
+            snprintf(line, sizeof(line), "input error\n");
+        
+        n = strlen(line);
+        Writen(sockfd, line, n);
+    }
 }
