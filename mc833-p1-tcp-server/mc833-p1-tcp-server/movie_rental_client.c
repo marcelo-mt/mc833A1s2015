2024c4aacdde27eefcdb44321adc7b491a26f684
Marcelo Mingatos <marcelo.m.toledo@gmail.com>
2015-04-23 06:26:32 -0300

    Test cases

--- mc833-p1-tcp-server/mc833-p1-tcp-server/movie_rental_client.c
+++ mc833-p1-tcp-server/mc833-p1-tcp-server/movie_rental_client.c
@@ -41,8 +41,6 @@ void movie_rental_client(FILE *fp, int sockfd)
             cVector(recvline);
         }
         
-        // pegar tempo
-        
         Fputs(recvline, stdout);
         
         printf("In: ");
@@ -51,39 +49,68 @@ void movie_rental_client(FILE *fp, int sockfd)
 
 void movie_rental_client_test(FILE *fp, int sockfd, Test test, int rep_count, struct timeval t1)
 {
-    //    char			sendline[MAXLINE];
     char            recvline[MAXLINE];
     MovieRentalRequest         request;
+    long totalSize;
+    
+    struct timeval t2;
+    long int elapsedTime;
+    int server_time;
     char *server_time_str;
     char sr_t[100];
-    int server_time;
     
-    //    while (Fgets(sendline, MAXLINE, fp) != NULL) {
+    switch (test) {
+        case Test2:
+            request.action = MovieRentalActionListTitleYearGenre;
+            request.parameter1 = MovieGenreAdventure;
+            break;
+        case Test3:
+            request.action = MovieRentalActionListTitleYearGenre;
+            request.parameter1 = 1;
+            break;
+        case Test4:
+            request.action = MovieRentalActionListTitleYearGenre;
+            request.parameter1 = 5;
+            break;
+        case Test5:
+            request.action = MovieRentalActionListTitleYearGenre;
+            break;
+        case Test6:
+            request.action = MovieRentalActionListTitleYearGenre;
+            request.parameter1 = 4;
+            request.parameter2 = 15;
+            break;
+        case Test7:
+            request.action = MovieRentalActionListTitleYearGenre;
+            request.parameter1 = 3;
+            break;
+        case Test1:
+        default:
+            request.action = MovieRentalActionListTitleYearGenre;
+            break;
+    }
     
-    //        if (sscanf(sendline, "%d%d%d", &request.action, &request.parameter1, &request.parameter2) <= 0) {
-    //            printf("invalid input: %s", sendline);
-    //            continue;
-    //        }
-    
-    request.action = MovieRentalActionListTitleYearGenre;
-    request.parameter1 = MovieGenreRomance;
-    request.timed = 0;
+    request.timed = 1;
     
     Writen(sockfd, &request, sizeof(request));
     
-    if (Readline(sockfd, recvline, MAXLINE) == 0)
-        err_quit("str_cli: server terminated prematurely");
+    while (1) {
+        if ((totalSize = (Readline(sockfd, recvline, MAXLINE))) == 0)
+            err_quit("movie_rental_client_test: server terminated prematurely");
+        
+        if(strstr(recvline, "--") != NULL) {
+            break;
+        }
+        
+//        Fputs(recvline, stdout);
+        cVector(recvline);
+    }
     
-    server_time_str = strrchr(recvline,'-');
+    server_time_str = strrchr(recvline,'|');
     server_time = atoi(server_time_str+1);
     snprintf(sr_t, sizeof(sr_t),"%d;", server_time);
     Fputs(sr_t, stdout);
     
-    //        Fputs(recvline, stdout);
-    //    }
-
-    struct timeval t2;
-    long int elapsedTime;
     
     // Termina o timer
     gettimeofday(&t2, NULL);
@@ -92,6 +119,8 @@ void movie_rental_client_test(FILE *fp, int sockfd, Test test, int rep_count, st
     elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000000;      // sec to us
     elapsedTime += (t2.tv_usec - t1.tv_usec);   // us
     
-    printf("%ld\n", elapsedTime);
+    snprintf(sr_t, sizeof(sr_t),"%ld\n", elapsedTime);
+    Fputs(sr_t, stdout);
+    
 }
 
